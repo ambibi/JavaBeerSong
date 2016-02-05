190dbfa3c52ac2766cbbeb4e777f2bbfe8cabb84
ambibi <abibi@oakland.edu>
2016-02-04 20:05:59 -0500

    Implemented the main and ninetyNineBottlesOfBeer methods

--- BeerSongJ/src/BeerSongJ.java
+++ BeerSongJ/src/BeerSongJ.java
@@ -1,9 +1,12 @@
-
+/*
+ * Author: Amanda
+ * Date: 2/4/2016
+ */
 public class BeerSongJ {
 
 	public static void main(String[] args) {
 		
-		
+		ninetyNineBottlesOfBeer();
 			
 				
 			}//End method main
@@ -13,6 +16,26 @@ public class BeerSongJ {
 	 */
 	
 	public static void ninetyNineBottlesOfBeer(){
+		int i;       //counter for loop
+		int num = 99;      // number  
+	    int newNum;        // Number-1
+	    newNum = num-1; 
+		
+		for (i=1; i<=97; i++)
+		{
+			System.out.println(num + " bottles of beer on the wall, " + num + " bottles of beer ");
+			System.out.println("take one down, pass it around, " + newNum + " bottles of beer on the wall. ");
+			
+			num = num-1;
+			newNum = newNum-1;
+			
+		} //End for
+		
+		
+			System.out.println("2 bottles of beer on the wall, 2 bottles of beer,\n take one down, pass it around, 1 bottle of beer on the wall. ");
+			System.out.println("1 bottle of beer on the wall, 1 bottle of beer, \n take one down, pass it around, no more bottles of beer on the wall. ");
+		
+
 		
 	} // End method ninetyNineBottlesOfBeer
 
